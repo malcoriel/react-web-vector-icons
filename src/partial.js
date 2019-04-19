b68eb18c1f6d4b31e01b24d3471ce95b714c83df
Valeriy Kuzmin <malcoriel@gmail.com>
2019-04-20 01:45:45 +0500

    fix: fix wrong set of icons in partial

--- src/partial.js
+++ src/partial.js
@@ -1,4 +1,4 @@
 import createIcon from './lib/create-icon-set';
 
 export const FontAwesome5 = createIcon(require('./glyphmaps/FontAwesome5Pro.json'), 'FontAwesome5_Solid');
-export const MaterialIcons = createIcon(require('./glyphmaps/MaterialIcons.json'), 'MaterialIcons');
+export const MaterialCommunityIcons = createIcon(require('./glyphmaps/MaterialCommunityIcons.json'), 'MaterialCommunityIcons');
